10b00fb30fc0cf157ec11fa1c123fa02f1e083dd
Nhien <phamnhien112004@gmail.com>
2026-08-29 20:07:06 -0500

    ignore esline and typesript during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    ...({
+        eslint: {
+            ignoreDuringBuilds: true,
+        },
+    } as Record<string, unknown>),
 };
 
 export default nextConfig;
